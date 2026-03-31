5e1fc0e5e59938dcb48ddbbfd32aa69336ce708a
wmranjit11-commits <wmranjit11@gmail.com>
2026-03-31 21:10:06 +0530

    Revert "Merge branch 'main' of https://github.com/Warrgyizmorsch/news-article"
    This reverts commit dab6b9835a9dcc90450abea5967d62b5d8e29b48, reversing
    changes made to ad0d412d658bd5e722d3c05684d4e054ee285f64.

--- resources/views/news/monthly-edition.blade.php
+++ resources/views/news/monthly-edition.blade.php
@@ -190,7 +190,7 @@
                                             <line x1="8" y1="2" x2="8" y2="6"></line>
                                             <line x1="3" y1="10" x2="21" y2="10"></line>
                                         </svg>
-                                        {{ optional($article->published_at)->format('M Y') }}
+                                        {{ optional($article->published_at)->format('M j, Y') }}
                                     </span>
                                 </div>
                                 
